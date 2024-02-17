0e2b089217266fb457a76b216f9eb7348c846eea
Muhammad Fauzan Aristya Putra <muhammadfauzanaristyaputra@gmail.com>
2024-02-17 08:55:32 +0300

    feat: implement typescript path aliasing

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import legacy from '@vitejs/plugin-legacy';
 import posthtml from '@vituum/vite-plugin-posthtml';
 import tailwindcss from '@vituum/vite-plugin-tailwindcss';
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 import vituum from 'vituum';
 
 export default defineConfig({
-	plugins: [vituum(), posthtml(), tailwindcss(), legacy()],
+	plugins: [vituum(), posthtml(), tailwindcss(), legacy(), tsconfigPaths()],
 	rollupOptions: {
 		minify: 'terser',
 		input: ['./src/pages/**/*.html'],
