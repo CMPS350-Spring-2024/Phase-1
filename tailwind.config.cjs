984cd0c9d2b247f54c96557891be76d77416a862
Muhammad Fauzan Aristya Putra <muhammadfauzanaristyaputra@gmail.com>
2024-03-01 10:18:08 +0300

    chore: setup `lucide` library

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,14 @@ export default {
 	important: true,
 	content: ['./src/**/*.{html,js,ts}', 'node_modules/preline/dist/*.js'],
 	theme: {
-		extend: {},
+		extend: {
+			strokeWidth: {
+				3: '3px',
+				4: '4px',
+				5: '5px',
+				6: '6px',
+			},
+		},
 	},
 	plugins: [require('@tailwindcss/forms'), require('preline/plugin')],
 };
