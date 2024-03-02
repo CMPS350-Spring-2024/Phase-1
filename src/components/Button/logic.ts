80f8b03721cb7eb15a44671d093d994d1b2d2f38
Muhammad Fauzan Aristya Putra <muhammadfauzanaristyaputra@gmail.com>
2024-03-02 10:59:53 +0300

    chore: setup VSCode autocomplete for custom components

--- src/components/Button/logic.ts
+++ src/components/Button/logic.ts
@@ -5,14 +5,40 @@ import { PrimitiveComponent } from '@/components/PrimitiveComponent';
 import type { BaseComponentProps } from '@/components/BaseComponent';
 
 export interface ButtonProps extends BaseComponentProps {
+	/**
+	 * Specifies the color of the button.
+	 */
 	color?: 'blue';
+
+	/**
+	 * Specifies the fill type of the button.
+	 */
 	fill?: 'solid' | 'outline' | 'ghost';
+
+	/**
+	 * Specifies the size of the button.
+	 */
 	size?: 'sm' | 'md' | 'lg';
+
+	/**
+	 * Makes the button have rounded corners.
+	 */
 	round?: boolean;
+
+	/**
+	 * Disables the button and prevents user interaction.
+	 */
 	disabled?: boolean;
+
+	/**
+	 * Shows a loading spinner inside the button.
+	 */
 	loading?: boolean;
 }
 
+/**
+ * Custom button component based on the Preline UI design system.
+ */
 export class Button extends PrimitiveComponent {
 	protected static readonly templateName: string = 'button-template';
 	protected static readonly forwardedAttributes: Array<keyof ButtonProps> = [
