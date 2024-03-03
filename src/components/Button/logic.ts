0a201ee7755fce680f3c32499e7ecb883d1b22aa
Muhammad Fauzan Aristya Putra <muhammadfauzanaristyaputra@gmail.com>
2024-03-03 06:45:02 +0300

    feat: add `href` prop to button component

--- src/components/Button/logic.ts
+++ src/components/Button/logic.ts
@@ -6,6 +6,11 @@ import type { BaseComponentProps } from '@/components/BaseComponent';
 
 export interface ButtonProps extends BaseComponentProps {
 	/**
+	 * Passes a href to the button, this will wrap the button in an anchor tag.
+	 */
+	href?: string;
+
+	/**
 	 * Specifies the color of the button.
 	 */
 	color?: 'blue';
@@ -42,6 +47,8 @@ export interface ButtonProps extends BaseComponentProps {
 export class Button extends PrimitiveComponent {
 	protected static readonly templateName: string = 'button-template';
 	protected static readonly forwardedAttributes: Array<keyof ButtonProps> = [
+		'href',
+
 		'class',
 		'color',
 		'fill',
@@ -61,6 +68,33 @@ export class Button extends PrimitiveComponent {
 			elementName: 'button',
 			defaultProperties: Button.defaultProperties,
 		});
+	}
+
+	attributeChangedCallback(name: string, oldValue: string, newValue: string): void {
+		super.attributeChangedCallback(name, oldValue, newValue);
+
+		//	If the href attribute is set, change the role to link if it's not already set, and wrap the button in an anchor tag
+		if (name === 'href') {
+			if (newValue === null) {
+				this.removeAttribute('role');
+
+				//	Unwrap the button from the anchor tag
+				const button = this.element.firstElementChild!.cloneNode(true) as HTMLElement;
+				this.element.insertAdjacentElement('afterend', button);
+				this.element.remove();
+				this.element = button;
+			} else {
+				this.setAttribute('role', 'link');
+
+				//	Wrap the button in an anchor tag
+				const anchor = this.element.ownerDocument!.createElement('a');
+				anchor.setAttribute('href', newValue);
+				anchor.appendChild(this.element.cloneNode(true));
+				this.element.insertAdjacentElement('beforebegin', anchor);
+				this.element.remove();
+				this.element = anchor;
+			}
+		}
 	}
 }
 
