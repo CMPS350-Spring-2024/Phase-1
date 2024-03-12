9419934a5e8dda65db3b1a3576d880a74be71764
Muhammad Fauzan Aristya Putra <muhammadfauzanaristyaputra@gmail.com>
2024-03-12 21:14:41 +0300

    feat: finish off base navbar styling

--- src/components/Navbar/logic.ts
+++ src/components/Navbar/logic.ts
@@ -1,25 +1,36 @@
 //	Component Imports
-import { BaseComponent } from '@/components/BaseComponent';
+import { PrimitiveComponent } from '@/components/PrimitiveComponent';
 
 //	Type Imports
 import type { BaseComponentProps } from '@/components/BaseComponent';
 
-export interface NavbarProps extends BaseComponentProps {}
+export interface Navbar extends NavbarProps {}
+export interface NavbarProps extends BaseComponentProps {
+	/**
+	 * The type of the current page
+	 */
+	type?: 'home' | 'customer' | 'admin';
+}
 
 /**
  * Custom navbar component based on the Preline UI design system.
  */
-export class Navbar extends BaseComponent {
+export class Navbar extends PrimitiveComponent {
 	protected static readonly templateName: string = 'navbar-template';
-	protected static readonly forwardedAttributes: Array<keyof NavbarProps> = [];
-	protected static readonly defaultProperties: NavbarProps = {};
+	protected static readonly forwardedAttributes: Array<keyof NavbarProps> = ['class', 'type'];
+	protected static readonly defaultProperties: NavbarProps = {
+		type: 'home',
+	};
 
 	static get observedAttributes() {
 		return [...super.observedAttributes, ...this.forwardedAttributes] as string[];
 	}
 
 	constructor() {
-		super({ defaultProperties: Navbar.defaultProperties });
+		super({
+			elementName: 'header',
+			defaultProperties: Navbar.defaultProperties,
+		});
 	}
 }
 
