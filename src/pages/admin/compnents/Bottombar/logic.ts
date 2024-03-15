f5a5fa969267f70441db9bf06f1b401a4e8b4bb2
Mohammed- ma1704262 <ma1704262@qu.edu.qa>
2024-03-15 10:03:09 +0300

    feat: top-nav-bar
    finished adding the top nav bar

--- src/pages/admin/compnents/Bottombar/logic.ts
+++ src/pages/admin/compnents/Bottombar/logic.ts
@@ -1,26 +1,26 @@
-//	Component Imports
-import { BaseComponent } from '@/components/BaseComponent';
+// //	Component Imports
+// import { BaseComponent } from '@/components/BaseComponent';
 
-//	Type Imports
-import type { BaseComponentProps } from '@/components/BaseComponent';
+// //	Type Imports
+// import type { BaseComponentProps } from '@/components/BaseComponent';
 
-export interface BottombarProps extends BaseComponentProps {}
+// export interface BottombarProps extends BaseComponentProps {}
 
-/**
- * Custom Bottombar component based on the Preline UI design system.
- */
-export class Bottombar extends BaseComponent {
-	protected static readonly templateName: string = 'Bottombar-template';
-	protected static readonly forwardedAttributes: Array<keyof BottombarProps> = [];
-	protected static readonly defaultProperties: BottombarProps = {};
+// /**
+//  * Custom Bottombar component based on the Preline UI design system.
+//  */
+// export class Bottombar extends BaseComponent {
+// 	protected static readonly templateName: string = 'Bottombar-template';
+// 	protected static readonly forwardedAttributes: Array<keyof BottombarProps> = [];
+// 	protected static readonly defaultProperties: BottombarProps = {};
 
-	static get observedAttributes() {
-		return [...super.observedAttributes, ...this.forwardedAttributes] as string[];
-	}
+// 	static get observedAttributes() {
+// 		return [...super.observedAttributes, ...this.forwardedAttributes] as string[];
+// 	}
 
-	constructor() {
-		super({ defaultProperties: Bottombar.defaultProperties });
-	}
-}
+// 	constructor() {
+// 		super({ defaultProperties: Bottombar.defaultProperties });
+// 	}
+// }
 
-customElements.define('ui-bottombar', Bottombar);
+// customElements.define('ui-bottombar', Bottombar);
