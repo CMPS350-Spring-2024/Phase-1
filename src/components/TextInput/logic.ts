79f3adcd1629f57ec39a33934af54275824f4b80
Muhammad Fauzan Aristya Putra <muhammadfauzanaristyaputra@gmail.com>
2024-03-15 09:39:53 +0300

    feat: add password visibility toggle

--- src/components/TextInput/logic.ts
+++ src/components/TextInput/logic.ts
@@ -55,6 +55,11 @@ export interface TextInputProps extends BaseComponentProps {
 	 * If this input is in a form, this property makes it required.
 	 */
 	required?: boolean;
+
+	/**
+	 * NOTE: This property should not be set by the user, it is used internally to determine if the input is a password input.
+	 */
+	isPassword?: boolean;
 }
 
 /**
@@ -75,11 +80,15 @@ export class TextInput extends PrimitiveComponent {
 		'disabled',
 		'readonly',
 		'required',
+
+		'isPassword',
 	];
 	protected static readonly defaultProperties: TextInputProps = {
 		type: 'text',
 		placeholder: 'Enter text...',
 	};
+
+	protected visibilityToggle: HTMLButtonElement | null = null;
 
 	static formAssociated = true;
 	private _defaultValue: string | undefined;
@@ -104,12 +113,21 @@ export class TextInput extends PrimitiveComponent {
 			defaultProperties: TextInput.defaultProperties,
 		});
 
-		//	Set private properties
+		//	Allow the input to be associated with a form
 		this._defaultValue = this.value;
 		this._internals = this.attachInternals();
 
+		//	If this is a password input, find the visibility toggle button and add the isPassword attribute
+		if (this.type === 'password') {
+			this.isPassword = true;
+			this.visibilityToggle = this.shadowRoot?.querySelector(
+				'[exportparts="root: visibility-toggle"]',
+			) as HTMLButtonElement;
+		}
+
 		//	Add event listener to update the value property when the input changes
 		this.element.addEventListener('change', () => this.handleValueChange());
+		this.visibilityToggle?.addEventListener('click', () => this.handleToggleVisibility());
 	}
 
 	connectedCallback(): void {
@@ -147,7 +165,7 @@ export class TextInput extends PrimitiveComponent {
 		this.value = value === '' ? undefined : value;
 
 		//	If the input is a password input, check if the value is valid
-		if (this.type === 'password') {
+		if (this.isPassword) {
 			const regex = new RegExp('^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$');
 			(this.element as HTMLInputElement).setCustomValidity(
 				regex.test(value) ? '' : (
@@ -158,6 +176,21 @@ export class TextInput extends PrimitiveComponent {
 
 		//	Update the internals
 		this.updateInternals();
+	};
+
+	private handleToggleVisibility = () => {
+		if (this.isPassword) {
+			const eyeIcon = `
+				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-eye"><path d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z"/><circle cx="12" cy="12" r="3"/></svg>
+			`;
+			const eyeOffIcon = `
+				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-eye-off"><path d="M9.88 9.88a3 3 0 1 0 4.24 4.24"/><path d="M10.73 5.08A10.43 10.43 0 0 1 12 5c7 0 10 7 10 7a13.16 13.16 0 0 1-1.67 2.68"/><path d="M6.61 6.61A13.526 13.526 0 0 0 2 12s3 7 10 7a9.74 9.74 0 0 0 5.39-1.61"/><line x1="2" x2="22" y1="2" y2="22"/></svg>
+			`;
+
+			const isHidden = this.type === 'password';
+			this.type = isHidden ? 'text' : 'password';
+			this.visibilityToggle!.innerHTML = isHidden ? eyeOffIcon : eyeIcon;
+		}
 	};
 
 	public checkValidity = () => this._internals.checkValidity();
