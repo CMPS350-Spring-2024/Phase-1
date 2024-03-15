a4728dc6e6acf0c157c80c3d58b1d595d4fdbf7c
Muhammad Fauzan Aristya Putra <muhammadfauzanaristyaputra@gmail.com>
2024-03-15 19:03:34 +0300

    refactor: move component imports to new file

---
+++ src/scripts/components.ts
@@ -0,0 +1,9 @@
+//	Component Imports
+import '@/components/Avatar/logic';
+import '@/components/Button/logic';
+import '@/components/Dropdown/logic';
+import '@/components/Modal/logic';
+import '@/components/Navbar/logic';
+import '@/components/NumericInput/logic';
+import '@/components/PriceDisplay/logic';
+import '@/components/TextInput/logic';
--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,16 +1,3 @@
-//	Package Imports
-import './icons';
-
-//	Component Imports
-import '@/components/Avatar/logic';
-import '@/components/Button/logic';
-import '@/components/Dropdown/logic';
-import '@/components/Modal/logic';
-import '@/components/Navbar/logic';
-import '@/components/NumericInput/logic';
-import '@/components/PriceDisplay/logic';
-import '@/components/TextInput/logic';
-
 //	Repository Imports
 import { UserRepository } from '@/scripts/db/UserRepository';
 
