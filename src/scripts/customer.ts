1b6d1f0f77461dfb61d13a4a7d50252531895eef
Muhammad Fauzan Aristya Putra <muhammadfauzanaristyaputra@gmail.com>
2024-03-16 08:52:02 +0300

    feat: disable redirects in development

--- src/scripts/customer.ts
+++ src/scripts/customer.ts
@@ -1,2 +1,2 @@
 //	If the user is not logged in or is not an customer, redirect to the login page
-if (!window.currentUser || window.currentUser.isAdmin) window.location.assign('/login.html');
+if (import.meta.env.PROD && (!window.currentUser || window.currentUser.isAdmin)) window.location.assign('/login.html');
