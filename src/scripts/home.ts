97cb4ad079df45a779fe53a5604fd5da79c1ab4d
Muhammad Fauzan Aristya Putra <muhammadfauzanaristyaputra@gmail.com>
2024-03-22 10:49:54 +0300

    feat: add view transitions for product details tab

--- src/scripts/home.ts
+++ src/scripts/home.ts
@@ -1,6 +1,37 @@
 //	Component Imports
 import '@/components/DroneViewer/logic';
 
+let viewDetailsButton: HTMLButtonElement;
+let backButton: HTMLButtonElement;
+
 document.addEventListener('DOMContentLoaded', () => {
-	// document.querySelector('main')!.classList.add('product-overview');
+	viewDetailsButton = document.querySelector('#view-details-cta') as HTMLButtonElement;
+	backButton = document.querySelector('#back-btn') as HTMLButtonElement;
+
+	viewDetailsButton.addEventListener('click', handleViewDetails);
+	backButton.addEventListener('click', handleBack);
 });
+
+const handleViewDetails = () => {
+	//	If the browser doesnt support view transitions
+	if (!document.startViewTransition) {
+		document.querySelector('main')!.classList.add('product-overview');
+		return;
+	}
+
+	document.startViewTransition(() => {
+		document.querySelector('main')!.classList.add('product-overview');
+	});
+};
+
+const handleBack = () => {
+	//	If the browser doesnt support view transitions
+	if (!document.startViewTransition) {
+		document.querySelector('main')!.classList.remove('product-overview');
+		return;
+	}
+
+	document.startViewTransition(() => {
+		document.querySelector('main')!.classList.remove('product-overview');
+	});
+};
