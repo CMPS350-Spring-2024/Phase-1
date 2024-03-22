b45235e3c93a481628d8a109ea5d917f7ba22fec
Muhammad Fauzan Aristya Putra <muhammadfauzanaristyaputra@gmail.com>
2024-03-22 16:53:09 +0300

    feat: add product details tab

--- src/scripts/home.ts
+++ src/scripts/home.ts
@@ -1,37 +1,59 @@
 //	Component Imports
+import { Button } from '@/components/Button/logic';
 import '@/components/DroneViewer/logic';
+
+let currentTab: number = 0;
 
 let viewDetailsButton: HTMLButtonElement;
 let backButton: HTMLButtonElement;
+let tabButtons: NodeListOf<Button>;
+let tabContents: NodeListOf<HTMLElement>;
 
 document.addEventListener('DOMContentLoaded', () => {
+	document.querySelector('main')!.classList.add('product-overview');
 	viewDetailsButton = document.querySelector('#view-details-cta') as HTMLButtonElement;
 	backButton = document.querySelector('#back-btn') as HTMLButtonElement;
+	tabButtons = document.querySelectorAll('.tab-list .tab');
+	tabContents = document.querySelectorAll('.tab-content');
 
 	viewDetailsButton.addEventListener('click', handleViewDetails);
 	backButton.addEventListener('click', handleBack);
+	tabButtons.forEach((button, index) => {
+		button.addEventListener('click', () => handleChangeTab(index));
+	});
 });
 
-const handleViewDetails = () => {
+const startViewTransition = (callback: () => void) => {
 	//	If the browser doesnt support view transitions
 	if (!document.startViewTransition) {
-		document.querySelector('main')!.classList.add('product-overview');
+		callback();
 		return;
 	}
-
-	document.startViewTransition(() => {
-		document.querySelector('main')!.classList.add('product-overview');
-	});
+	document.startViewTransition(callback);
 };
 
-const handleBack = () => {
-	//	If the browser doesnt support view transitions
-	if (!document.startViewTransition) {
-		document.querySelector('main')!.classList.remove('product-overview');
-		return;
-	}
+const handleViewDetails = () =>
+	startViewTransition(() => {
+		document.querySelector('main')!.classList.add('product-overview');
+	});
 
-	document.startViewTransition(() => {
+const handleBack = () =>
+	startViewTransition(() => {
 		document.querySelector('main')!.classList.remove('product-overview');
 	});
+
+const handleChangeTab = (index: number) => {
+	const previousTab = currentTab;
+	currentTab = index;
+
+	//	If the tab is already active, return
+	if (previousTab === index) return;
+
+	//	Remove active class from previous tab
+	tabButtons[previousTab].classList.remove('active');
+	tabContents[previousTab].classList.remove('active');
+
+	//	Add active class to new tab
+	tabButtons[index].classList.add('active');
+	tabContents[index].classList.add('active');
 };
